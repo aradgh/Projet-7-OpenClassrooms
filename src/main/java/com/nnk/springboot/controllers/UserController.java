bfc1839d8a3d893a88fea976daa830a535aa2af5
aradgh <arad.ghorreshi@gmail.com>
2025-07-21 10:54:07 +0200

    Enhance UserController with role-based access, password encoding, and error handling
    Introduced role-based access control for user list view, redirecting non-admin users to a 403 page. Updated user validation to encode passwords before saving. Improved form handling by adding model attributes for `addUser`.

--- src/main/java/com/nnk/springboot/controllers/UserController.java
+++ src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -3,39 +3,59 @@ package com.nnk.springboot.controllers;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @RequestMapping("/user/list")
-    public String home(Model model) {
-        model.addAttribute("users", userService.findAll());
-        return "user/list";
+    public String home(Model model, @AuthenticationPrincipal UserDetails currentUser) {
+        String username = currentUser.getUsername();
+        User user = userService.findByUsername(username);
+
+        if(user.getRole().equals("ADMIN")) {
+            model.addAttribute("users", userService.findAll());
+            return "user/list";
+        }
+        return "redirect:/403";
+    }
+
+    @RequestMapping("/403")
+    public String error403() {
+        return "403";
     }
 
     @GetMapping("/user/add")
-    public String addUser(User user) {
+    public String addUser(Model model, User user) {
+        model.addAttribute("user", user);
         return "user/add";
     }
 
     @PostMapping("/user/validate")
-    public String validate(@Valid User user, BindingResult result, Model model) {
-        if (!result.hasErrors()) {
-            userService.save(user);
-            return "redirect:/user/list";
+    public String validate(@Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "user/add";
         }
-        return "user/add";
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.save(user);
+        return "redirect:/user/list";
     }
 
     @GetMapping("/user/update/{id}")
